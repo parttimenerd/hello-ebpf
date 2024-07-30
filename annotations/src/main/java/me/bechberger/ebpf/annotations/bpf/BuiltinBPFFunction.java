e2d3ebba744bd271e5d447ad72dd1be7c1ce6bb8
Johannes Bechberger <johannes.bechberger@sap.com>
2024-07-30 16:43:51 +0200

    Small doc fix

--- annotations/src/main/java/me/bechberger/ebpf/annotations/bpf/BuiltinBPFFunction.java
+++ annotations/src/main/java/me/bechberger/ebpf/annotations/bpf/BuiltinBPFFunction.java
@@ -36,7 +36,7 @@ public @interface BuiltinBPFFunction {
      *     <li>{@code $argsN_}: Arguments N to the last argument, comma separated</li>
      *     <li>{@code $this}: The object the method is called on</li>
      *     <li>{@code $T1, $T2, ...}: Type parameters</li>
-     *     <li>{@code $C1, $T2, ...}: Type parameters of the type of {@code $this}</li>
+     *     <li>{@code $C1, $C2, ...}: Type parameters of the type of {@code $this}</li>
      *     <li>{@code $strlen$this}: length of the {@code $this} interpreted as a string literal</li>
      *     <li>{@code $strlen$argN}: length of the {@code $argN} interpreted as a string literal</li>
      *     <li>{@code $str$argN}: Asserts that {@code $argN} is a string literal</li>
@@ -61,6 +61,12 @@ public @interface BuiltinBPFFunction {
      *    func(1, 2, 3)
      *    // will be translated to
      *    func(1, 2, 3, 1)
+     *
+     *    @BuiltinBPFFunction("func($str$arg1, sizeof($arg1))")
+     *    void func(String a);
+     *    func("abc")
+     *    // will be translated to
+     *    func("abc", sizeof("abc"))
      * }
      */
     String value() default "$name($args)";
