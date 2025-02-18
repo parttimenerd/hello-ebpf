8d4e8f3f2c7e8deead38258feefa6beb17ea4ba9
Mr-Pine <git@mr-pine.de>
2025-02-19 00:09:06 +0100

    Convert FEntryExitAutoAttachTest to RingBuffer

--- bpf/src/test/java/me/bechberger/ebpf/bpf/FEntryExitAutoAttachTest.java
+++ bpf/src/test/java/me/bechberger/ebpf/bpf/FEntryExitAutoAttachTest.java
@@ -1,13 +1,22 @@
 package me.bechberger.ebpf.bpf;
 
+import me.bechberger.ebpf.annotations.Size;
 import me.bechberger.ebpf.annotations.bpf.BPF;
+import me.bechberger.ebpf.annotations.bpf.BPFFunction;
+import me.bechberger.ebpf.annotations.bpf.BPFMapDefinition;
+import me.bechberger.ebpf.bpf.map.BPFRingBuffer;
+import me.bechberger.ebpf.runtime.OpenDefinitions;
 import me.bechberger.ebpf.shared.TraceLog;
+import me.bechberger.ebpf.type.Ptr;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
+import static me.bechberger.ebpf.bpf.BPFJ.bpf_probe_read_user_str;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -22,38 +31,35 @@ public class FEntryExitAutoAttachTest {
     public static abstract class OpenAt extends BPFProgram {
 
         final GlobalVariable<Integer> targetPid = new GlobalVariable<>(0);
+
         private static final String SYS_PREFIX = "/sys/";
+
+        @BPFMapDefinition(maxEntries = 1024)
+        BPFRingBuffer<@Size(64) String> pathBuffer;
+
+        @BPFFunction(
+                section = "fentry/do_sys_openat2",
+                headerTemplate = "int BPF_PROG($name, $params)",
+                autoAttach = true
+        )
+        int doOpenat2(long dfd, String name, Ptr<OpenDefinitions.open_how> how) {
+            String path = pathBuffer.reserve().asString();
+            if (path == null) {
+                return 0;
+            }
+            bpf_probe_read_user_str(path, 64, name);
+            pathBuffer.submit(Ptr.of(path).<String>cast());
+            return 0;
+        }
 
         static final String EBPF_PROGRAM = """
                 #include "vmlinux.h"
                 #include <bpf/bpf_helpers.h>
                 #include <bpf/bpf_tracing.h>
                 
-                SEC("fentry/do_sys_openat2")
-                int BPF_PROG(do_openat2, long dfd, const u8* name, struct open_how *how)
-                {
-                	char name_copy[sizeof(SYS_PREFIX)];
-                	BPF_SNPRINTF(name_copy, sizeof(name_copy), "%s", name);
-                	bool is_sys = bpf_strncmp(name_copy, sizeof(SYS_PREFIX), (const u8*) SYS_PREFIX) == 0;
-                	pid_t pid;
-                	pid = bpf_get_current_pid_tgid() >> 32;
-                	if (pid == targetPid && !is_sys) {
-                	    bpf_printk("fentry: pid = %d, filename = %s", pid, name);
-                	}
-                	return 0;
-                }
-                
                 SEC("fexit/do_sys_openat2")
                 int BPF_PROG(do_openat2_exit, long dfd, const char *name, struct open_how *how, long ret)
                 {
-                	char name_copy[sizeof(SYS_PREFIX)];
-                	BPF_SNPRINTF(name_copy, sizeof(name_copy), "%s", name);
-                	bool is_sys = bpf_strncmp(name_copy, sizeof(SYS_PREFIX), (const u8*) SYS_PREFIX) == 0;
-                	pid_t pid;
-                	pid = bpf_get_current_pid_tgid() >> 32;
-                	if (pid == targetPid && !is_sys) {
-                	    bpf_printk("fexit: pid = %d, filename = %s", pid, name);
-                	}
                 	return 0;
                 }
                 
@@ -67,26 +73,30 @@ public class FEntryExitAutoAttachTest {
 
     @Test
     public void testOpenAt() throws IOException {
-        Path testFile;
+        Path testFile = Path.of("");
+        List<String> files = new ArrayList<>();
         try (var program = BPFProgram.load(OpenAt.class)) {
             program.autoAttachPrograms();
             program.targetPid.set((int) ProcessHandle.current().pid());
+            program.pathBuffer.setCallback(path -> {
+                files.add(path);
+            });
             testFile = TestUtil.triggerOpenAt();
+            try {
+                program.pathBuffer.consume();
+            } catch (Exception e) {
+                // Ignore
+            }
+            assertTrue(files.contains(testFile.toString()));
         }
-        var entryLine = TraceLog.getInstance().readLine();
-        assertTrue(entryLine.contains("fentry"), entryLine);
-        assertTrue(entryLine.contains(testFile.toString()), entryLine);
-        var exitLine = TraceLog.getInstance().readLine();
-        assertTrue(exitLine.contains("fexit"), exitLine);
-        assertTrue(exitLine.contains(testFile.toString()), exitLine);
 
-        var line = TraceLog.getInstance().readAllAvailableLines();
+        TraceLog.getInstance().readAllAvailableLines();
     }
 
     @Test
     public void testAutoAttachAll() {
         try (var program = BPFProgram.load(OpenAt.class)) {
-            assertEquals(List.of("do_openat2", "do_openat2_exit", "kprobe__do_sys_openat2"), program.getAutoAttachablePrograms());
+            assertEquals(Stream.of("doOpenat2", "do_openat2_exit", "kprobe__do_sys_openat2").sorted().toList(), program.getAllAutoAttachablePrograms().stream().sorted().toList());
         }
     }
 }
