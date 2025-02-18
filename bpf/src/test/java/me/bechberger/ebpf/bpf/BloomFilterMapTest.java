57f314043d9f422a678b14193991c92f714bf739
Mr-Pine <git@mr-pine.de>
2025-02-18 22:19:50 +0100

    Convert BloomFilterMapTest to RingBuffer

--- bpf/src/test/java/me/bechberger/ebpf/bpf/BloomFilterMapTest.java
+++ bpf/src/test/java/me/bechberger/ebpf/bpf/BloomFilterMapTest.java
@@ -1,88 +1,109 @@
 package me.bechberger.ebpf.bpf;
 
+import me.bechberger.ebpf.annotations.Type;
 import me.bechberger.ebpf.annotations.bpf.BPF;
+import me.bechberger.ebpf.annotations.bpf.BPFFunction;
 import me.bechberger.ebpf.annotations.bpf.BPFMapDefinition;
 import me.bechberger.ebpf.bpf.map.BPFBloomFilter;
+import me.bechberger.ebpf.bpf.map.BPFRingBuffer;
+import me.bechberger.ebpf.runtime.PtDefinitions;
 import me.bechberger.ebpf.shared.TraceLog;
+import me.bechberger.ebpf.type.Enum;
+import me.bechberger.ebpf.type.Ptr;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BloomFilterMapTest {
 
+    public static final int PLACED = 12;
+
     @BPF(license = "GPL")
     public static abstract class Program extends BPFProgram {
+        @Type
+        protected enum ResultPoint implements Enum<ResultPoint> {
+            PLACED, BEFORE, AFTER
+        }
+
+        @Type
+        protected record Result(
+                ResultPoint point,
+                boolean ok
+        ) {
+        }
+
+        @BPFMapDefinition(maxEntries = 4)
+        BPFRingBuffer<Result> results;
+
         @BPFMapDefinition(maxEntries = 256)
         BPFBloomFilter<Integer> filter;
 
-        static final String EBPF_PROGRAM = """
-            #include <vmlinux.h>
-            #include <bpf/bpf_helpers.h>
-            #include <bpf/bpf_endian.h>
 
-            SEC ("kprobe/do_sys_openat2")
-                 int kprobe__do_sys_openat2 (struct pt_regs *ctx)
-            {
-              int placed_value = 12;
-              bool placed_value_in_filter = !bpf_map_peek_elem(&filter, &placed_value);
-              if (placed_value_in_filter) {
-                bpf_printk("Placed OK");
-              } else {
-                bpf_printk("Placed NOT OK");
-              }
-
-              int value = 11;
-              bool maybe_in_filter = !bpf_map_peek_elem(&filter, &value);
-              if (!maybe_in_filter) {
-                bpf_printk("Before OK");
-              } else {
-                bpf_printk("Before NOT OK");
-              }
-
-              bpf_map_push_elem(&filter, &value, BPF_ANY);
-              maybe_in_filter = !bpf_map_peek_elem(&filter, &value);
-              if (maybe_in_filter) {
-                bpf_printk("After OK");
-              } else {
-                bpf_printk("After NOT OK");
-              }
-              return 0;
+        @BPFFunction(
+                section = "kprobe/do_sys_openat2",
+                autoAttach = true
+        )
+        int testFilter(Ptr<PtDefinitions.pt_regs> ctx) {
+            int placedValue = PLACED;
+            Ptr<Result> result = results.reserve();
+            if (result != null) {
+                Ptr.of(result.val().point).set(ResultPoint.PLACED);
+                Ptr.of(result.val().ok).set(filter.peek(placedValue));
+                results.submit(result);
             }
-        """;
+
+            int value;
+            for (value = 0; value < 10; value++) {
+                int tmp_value = value;
+                if (!filter.peek(tmp_value)) {
+                    break;
+                }
+            }
+
+            result = results.reserve();
+            if (result != null) {
+                Ptr.of(result.val().point).set(ResultPoint.BEFORE);
+                Ptr.of(result.val().ok).set(!filter.peek(value));
+                results.submit(result);
+            }
+
+            filter.put(value);
+
+            result = results.reserve();
+            if (result != null) {
+                Ptr.of(result.val().point).set(ResultPoint.AFTER);
+                Ptr.of(result.val().ok).set(filter.peek(value));
+                results.submit(result);
+            }
+
+            return 0;
+        }
     }
 
     @Test
-    public void testBasicBloomFilter() throws InterruptedException {
+    public void testBasicBloomFilter() {
         try (var program = BPFProgram.load(BloomFilterMapTest.Program.class)) {
+            program.autoAttachPrograms();
             var filter = program.filter;
-            filter.put(12);
-            program.autoAttachProgram(program.getProgramByName("kprobe__do_sys_openat2"));
+            filter.put(PLACED);
             TestUtil.triggerOpenAt();
 
-            while (true) {
-                var msg = program.readTraceFields().msg();
-                if (msg != null && msg.contains("Placed")) {
-                    assertEquals("Placed OK", msg);
-                    break;
-                }
+            List<Program.Result> results = new ArrayList<>();
+
+            program.results.setCallback(result -> {
+                results.add(result);
+            });
+
+            while (results.size() < 3) {
+                program.results.consumeAndThrow();
             }
 
-            while (true) {
-                var msg = program.readTraceFields().msg();
-                if (msg != null && msg.contains("Before")) {
-                    assertEquals("Before OK", msg);
-                    break;
-                }
-            }
-
-            while (true) {
-                var msg = program.readTraceFields().msg();
-                if (msg != null && msg.contains("After")) {
-                    assertEquals("After OK", msg);
-                    break;
-                }
+            for (var result : results) {
+                assertTrue(result.ok, result.toString());
             }
         }
         TraceLog.getInstance().readAllAvailableLines(Duration.ofMillis(100));
