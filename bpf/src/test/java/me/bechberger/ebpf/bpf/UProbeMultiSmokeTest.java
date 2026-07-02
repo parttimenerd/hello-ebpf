0eb9e5613e2cbe0cf5b61dec55c58ef1802d3bc3
Johannes Bechberger <johannes.bechberger@sap.com>
2026-07-03 00:28:05 +0200

    test(bpf): kprobe.multi and uprobe.multi vng smoke tests

---
+++ bpf/src/test/java/me/bechberger/ebpf/bpf/UProbeMultiSmokeTest.java
@@ -0,0 +1,62 @@
+package me.bechberger.ebpf.bpf;
+
+import me.bechberger.ebpf.annotations.bpf.BPF;
+import me.bechberger.ebpf.annotations.bpf.BPFFunction;
+import me.bechberger.ebpf.annotations.bpf.BPFMapDefinition;
+import me.bechberger.ebpf.annotations.bpf.UProbeMulti;
+import me.bechberger.ebpf.bpf.map.BPFHashMap;
+import me.bechberger.ebpf.type.Ptr;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UProbeMultiSmokeTest {
+
+    @BPF(license = "GPL")
+    public static abstract class UProg extends BPFProgram {
+        static final String EBPF_PROGRAM = """
+                #include <vmlinux.h>
+                #include <bpf/bpf_helpers.h>
+                """;
+
+        @BPFMapDefinition(maxEntries = 32)
+        BPFHashMap<Long, Long> hits;
+
+        @BPFFunction(section = "uprobe.multi/dummy", autoAttach = false)
+        @UProbeMulti("*")
+        int onMany(Ptr<?> ctx) {
+            long cookie = BPFJ.bpf_get_attach_cookie(ctx);
+            Ptr<Long> cur = hits.bpf_get(cookie);
+            long next;
+            if (cur == null) {
+                next = 1;
+            } else {
+                next = cur.val() + 1;
+            }
+            hits.put(cookie, next);
+            return 0;
+        }
+    }
+
+    @Test
+    void multiAttachThreeLibcFuncsAndTriggerFromChild() throws Exception {
+        String binaryPath = "/usr/lib/x86_64-linux-gnu/libc.so.6";
+        String[] funcs   = { "malloc", "free", "getenv" };
+        long[]   cookies = { 0x11L, 0x22L, 0x33L };
+
+        try (var prog = BPFProgram.load(UProg.class)) {
+            prog.attachUprobeMulti(prog.getProgramByName("onMany"),
+                    binaryPath, funcs, cookies, false);
+            // /bin/ls hits all three of malloc/free/getenv.
+            Runtime.getRuntime().exec(new String[]{"/bin/ls", "/"}).waitFor();
+
+            int matched = 0;
+            for (long c : cookies) {
+                Long v = prog.hits.get(c);
+                if (v != null && v > 0) matched++;
+            }
+            assertEquals(3, matched,
+                    "all three libc functions should have fired at least once");
+        }
+    }
+}
